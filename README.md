184356ece440c0f03a36bd3eeb4c6f26dc680b23
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-09-13 14:16:06 +0300

    build: init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Demo](http://github.com)
+
+## Menu with creative animation and hover effect
+
+### React + TypeScript + Vite
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import MenuList from "./components/menu_list";
+
+const menuItems = ["Home", "About", "Contact", "Services", "Blog"];
+
+function App() {
+  return <MenuList items={menuItems} />;
+}
+
+export default App;
