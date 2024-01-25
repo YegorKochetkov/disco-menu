116757de263bc954e87e7c0439fa0fc5b21eb453
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-01-25 23:59:04 +0200

    refactor: remove index for key, fix href

--- src/components/menu_item/menu_item.tsx
+++ src/components/menu_item/menu_item.tsx
@@ -1,39 +1,39 @@
 import classes from "./menu_item.module.css";
 
 function MenuItem({
-  item,
-  colorVariable,
+	item,
+	colorVariable,
 }: {
-  item: string;
-  colorVariable?: string;
+	item: string;
+	colorVariable?: string;
 }) {
-  const formattedItem = item.split("").map((item, index) => {
-    return (
-      <span
-        key={index}
-        style={
-          {
-            "--animation-duration": `${Math.random() * item.length + 0.5}s`,
-          } as React.CSSProperties
-        }
-      >
-        {item}
-      </span>
-    );
-  });
+	const formattedItem = item.split("").map((item) => {
+		return (
+			<span
+				key={item}
+				style={
+					{
+						"--animation-duration": `${Math.random() * item.length + 0.5}s`,
+					} as React.CSSProperties
+				}
+			>
+				{item}
+			</span>
+		);
+	});
 
-  const cssColorVariable = {
-    "--clr": `var(${colorVariable}, var(--clr-fallback))`,
-  } as React.CSSProperties;
+	const cssColorVariable = {
+		"--clr": `var(${colorVariable}, var(--clr-fallback))`,
+	} as React.CSSProperties;
 
-  return (
-    <li className={classes.item}>
-      <a href='#' style={cssColorVariable}>
-        <span className={classes.colored_text}>&nbsp;{formattedItem}</span>
-        <span className={classes.animated_text}>&nbsp;{formattedItem}</span>
-      </a>
-    </li>
-  );
+	return (
+		<li className={classes.item}>
+			<a href="/" style={cssColorVariable}>
+				<span className={classes.colored_text}>&nbsp;{formattedItem}</span>
+				<span className={classes.animated_text}>&nbsp;{formattedItem}</span>
+			</a>
+		</li>
+	);
 }
 
 export default MenuItem;
--- src/components/menu_list/menu_list.tsx
+++ src/components/menu_list/menu_list.tsx
@@ -2,13 +2,13 @@ import MenuItem from "../menu_item/menu_item";
 import classes from "./menu_list.module.css";
 
 function MenuList({ items }: { items: string[] }) {
-  return (
-    <ul className={classes.list}>
-      {items.map((item, index) => (
-        <MenuItem key={index} item={item} colorVariable={`--clr${index + 1}`} />
-      ))}
-    </ul>
-  );
+	return (
+		<ul className={classes.list}>
+			{items.map((item, index) => (
+				<MenuItem key={item} item={item} colorVariable={`--clr${index + 1}`} />
+			))}
+		</ul>
+	);
 }
 
 export default MenuList;
